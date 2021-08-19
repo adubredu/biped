87e54877b91d0471615d9df310cfbb7f00a3da74
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2021-08-19 02:23:05 -0400

    fixed kinematics plotting

--- kinematics/draw_links.jl
+++ kinematics/draw_links.jl
@@ -3,15 +3,16 @@ using GeometryTypes, MeshIO, FileIO, Rotations
 using GeometryBasics: HyperRectangle, Vec, Point, Mesh, Line
 using Colors: RGBA, RGB
 using Plots
+import PyPlot
 
-
-function draw_arm(link_endpoints)
-    closeall()
+function draw_arm(link_endpoints) 
     p = nothing
+    plot()
     for coords in link_endpoints
         xs = [coords[i][1] for i=1:length(coords)]
         ys = [coords[i][2] for i=1:length(coords)]
         p = plot!(xs, ys, xlims=(-2,2), ylims=(-2,2), aspect_ratio=:equal, legend=false)
+
     end
     p
 end
--- kinematics/kin.jl
+++ kinematics/kin.jl
@@ -2,6 +2,7 @@ include("fk.jl")
 include("ik.jl")
 using Plots
 import Plots
+import PyPlot
 
 phis = range(0, stop=2pi, length=50)
 c = [1,0.5]
@@ -12,13 +13,14 @@ yref = c[2].+r*sin.(phis)
 anim = Plots.Animation()
 traj = []
 for i = 1:50
+    PyPlot.clf()
     th1, th2 = solve_ik(xref[i], yref[i])
     p, q = solve_fk(th1, th2)
-    draw_arm([[[0,0], p], [p, q]])
+    pl = draw_arm([[[0,0], p], [p, q]])
     frame(anim)
     push!(traj, [th1, th2])
-    closeall()
+
 end
 
 println(traj)
-gif(anim, "circle.gif", fps=15)
+gif(anim, "kinematics/circle.gif", fps=15)
